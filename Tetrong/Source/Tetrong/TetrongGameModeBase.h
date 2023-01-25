76ebfb45a97b7df3dcbfd45ce3156ef3c93ce441
Kostas Ar <conarvanitis@hotmail.com>
2023-01-25 13:52:27 +0000

    [Tetrong] - Initial Project Commit

---
+++ Tetrong/Source/Tetrong/TetrongGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TetrongGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRONG_API ATetrongGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
